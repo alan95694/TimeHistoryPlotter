f8a64cde8ec195bd8895c04be34611ede49ee557
alan95694 <alan.c.wilkening@gmail.com>
2025-06-25 16:30:34 -0400

    remove loaded files

---
+++ +guiControl/deleteFile.m
@@ -0,0 +1,23 @@
+function deleteFile(app)
+% 
+% 
+% 
+
+
+if isempty(app.LoadedFilesListBox.Items) 
+    return
+end
+
+iFile  = find(strcmp( ...
+    app.LoadedFilesListBox.Items, ...
+    app.LoadedFilesListBox.Value) );
+
+%% Remove data loaded data structure
+app.tHData = rmfield(app.tHData, app.LoadedFilesListBox.Value);
+
+%% Update list box
+app.LoadedFilesListBox.Items(iFile) = [];
+
+
+end
+%% =======================================================================================

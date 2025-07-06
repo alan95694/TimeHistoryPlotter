94b8d35613c56c7854568bb323f0e5e2ff0eef78
alan95694 <alan.c.wilkening@gmail.com>
2025-07-05 21:05:27 -0400

    delete all templates

--- +guiControl/deleteTemplate.m
+++ +guiControl/deleteTemplate.m
@@ -3,6 +3,24 @@ function deleteTemplate(app, optDeleteAll)
 % 
 % 
 
+
+if optDeleteAll
+    hTmps = app.TmpTree.Children;
+    
+    for hCur = hTmps'
+        app.TmpTree.SelectedNodes = hCur;
+        deleteDelected(app);
+    end
+else
+    deleteDelected(app);
+end
+   
+
+
+end
+%% =======================================================================================
+function deleteDelected(app)
+
 [hh] = guiControl.getNodeLocation(app, app.TmpTree.SelectedNodes);
 
 % Get selected template
